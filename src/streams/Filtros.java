4461cff8e235986c0ac415a7e8b7b85ef1866132
Omar Villalpando <g_omar_v@live.com.mx>
2017-08-07 02:46:02 -0500

    Stream java 8
    Nuevos ejemplos de uso de filtros con Stream java 8

---
+++ src/streams/Filtros.java
@@ -0,0 +1,58 @@
+package streams;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Filtros {
+	public static void main(String[] args) throws IOException {
+		BufferedReader libro = new BufferedReader(
+				new FileReader("textosEjemplo/alicia.txt"));
+
+		List<String> palabras = new ArrayList<>();
+
+		String textoTMP = "";
+		while ((textoTMP = libro.readLine()) != null) {
+			String[] palabrasTMP = textoTMP.split(" ");
+			for (String palabraTMP : palabrasTMP) {
+				if (!(palabraTMP.equals(" ") || palabraTMP.equals(""))) {
+					System.out.println(palabraTMP);
+					palabras.add(palabraTMP);
+				}
+			}
+		}
+
+		Stream<String> filtradoPalabrasL = palabras.stream().filter(u -> u.startsWith("l"));
+		List<String> palabrasConL = filtradoPalabrasL.collect(Collectors.toList());
+		for (String palabraL : palabrasConL) {
+			System.out.println(palabraL);
+		}
+		filtradoPalabrasL = palabras.stream().filter(u -> u.startsWith("l"));
+
+		long contadorPalabrasL = filtradoPalabrasL.count();
+		System.out.println("Numero de palabras con L: " + contadorPalabrasL);
+
+		Predicate<String> palabrasConNPredicate = new Predicate<String>() {
+			@Override
+			public boolean test(String txt) {
+				return txt.toUpperCase().startsWith("N");
+			}
+		};
+
+		Stream<String> filtradoPalabrasN = palabras.stream().filter(palabrasConNPredicate);
+
+		List<String> palabrasConN = filtradoPalabrasN.collect(Collectors.toList());
+		for (String palabra : palabrasConN) {
+			System.out.println(palabra);
+		}
+		filtradoPalabrasN = palabras.stream().filter(palabrasConNPredicate);
+		long contadorPalabrasN = filtradoPalabrasN.count();
+		System.out.println("Numero de palabras con N: " + contadorPalabrasN);
+
+	}
+}
